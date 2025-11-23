6bb87e8c3ee4944c548687a575ddb676ac3cc331
muh-dixon <shabil.dixon@gmail.com>
2025-11-22 23:13:30 -0500

    changes to ui for mobile users

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Books from "./pages/Books";
